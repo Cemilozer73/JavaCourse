27be94c1c4ab8bacad8dcb7726629cbaf7b89372
Cemilozer73 <cemilozer73@gmail.com>
2023-08-06 16:09:45 +0300

    30.Gun

---
+++ src/C02_LocalTime.java
@@ -0,0 +1,7 @@
+public class C02_LocalTime {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/day28_constructorCall/C01_ConstructorCall.java
+++ src/day28_constructorCall/C01_ConstructorCall.java
@@ -1,6 +1,22 @@
 package day28_constructorCall;
 
+
 public class C01_ConstructorCall {
+    /*
+
+       CONSTRUCTOR CALL ÖNEMLİ NOTLAR:
+
+       1-) Main Method'un icinde constructor olusturmak icin "new ClassIsmi(ilgili argument)" seklinde constructor'ı yazabiliriz.
+       2-) Main Method'un icinde Method yazmak icin direk ismini yazabiliyoruz. ===> "ClassIsmi(ilgiliargument)" Methodu'dur diyebiliriz.
+       3-) Bir constuctor'ın icinde constructor call yapmak istersek bunun 2 şartı vardır:
+           1. this() şeklinde yazıp varsa parametresini parantez içinde belirtirsek
+              ilgili constructor'ı çalıştırmak üzere çağırırız.
+           2. Birde this() constructor'ın içinde sadece ilk satırda yazılmalıdır.
+              Sonraki satırlar için cte hatası verir.
+              Bu nedenle de bir constructor'ın içinde birden fazla constructor call yapilamaz.
+       4-) Eğer constructor'ın içinde ClassIsmi(ilgiliargument) seklinde bir call yapılmışsa bu Method Call'dur demeliyiz.
+
+      */
     C01_ConstructorCall(){
         System.out.println("Parametresiz constructor calisti");
     }
